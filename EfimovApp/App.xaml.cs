84291dce031863dfaf9bba0cf59eb3a2695b605f
KaterinaOhot <94222027+KaterinaOhot@users.noreply.github.com>
2022-12-23 13:07:02 +0300

    Добавьте файлы проекта.

---
+++ EfimovApp/App.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace EfimovApp
+{
+    /// <summary>
+    /// Логика взаимодействия для App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        
+
+    }
+    
+
+        
+        
+       
+
+    
+}
---
+++ EfimovApp/GuestWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace EfimovApp
+{
+    /// <summary>
+    /// Логика взаимодействия для GuestWindow.xaml
+    /// </summary>
+    public partial class GuestWindow : Window
+    {
+        public GuestWindow()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Возврат на главную страницу
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void GuestBackButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+            MainWindow mainWindow = new MainWindow();
+            mainWindow.Show();
+        }
+    }
+}
